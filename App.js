73c94f0456d92bbb16342b96aac22194aa602cba
Ian Hancock <northroadian@gmail.com>
2022-10-10 06:20:13 -0500

    setup

---
+++ App.js
@@ -0,0 +1,56 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import MapView from "react-native-maps";
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+
+
+  return (
+    <View style={styles.container}>
+    {/*Render our MapView*/}
+      <MapView
+        style={styles.map}
+        //specify our coordinates.
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1, //the container will fill the whole screen.
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default App;
